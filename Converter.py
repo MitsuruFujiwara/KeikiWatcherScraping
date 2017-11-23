8e22b135e16cbe48829134cb8d8fcb9e24c9a8af
MitsuruFujiwara <fujiwara52jp@gmail.com>
2017-11-23 16:53:09 +0900

    コメントを追加

--- Converter.py
+++ Converter.py
@@ -13,23 +13,29 @@ class ConverterBase(object):
         self.col_to_use = None
 
     def getData(self, filepath):
+        # 変換するファイルを開く
         with codecs.open(filepath, "r", "Shift-JIS", "ignore") as file:
             df = pd.read_table(file, delimiter=",")
 
+        # 不要な列を削除し、カラム名を指定
         df = df.ix[8:, :len(self.col)]
         df.columns = self.col
         df = df[self.col_to_use]
 
+        # 不要な行を削除
         df = df.replace('＊', np.nan)
         df = df.replace('−', np.nan)
         df = df[df.Label != '景気の現状判断']
         df = df[df.Label != '景気の先行き判断']
         df = df.dropna()
+
+        #  景気判断理由文頭の・を削除
         df.Comment = list(self.getComment(df.Comment))
 
         return df
 
     def getComment(self, comment):
+        #  景気判断理由文頭の・を削除するための関数
         for c in comment:
             yield c[1:]
 
@@ -54,6 +60,7 @@ class ConverterSakiyuki(ConverterBase):
         self.col_to_use = ['Label', 'Job', 'Comment']
 
 if __name__ == '__main__':
+    # test
     cg = ConverterGenjou('watcher4.csv')
     print(cg.getData())
 
